fa93f7a93318025cb471e2a458b0999baf83c8e0
neocentrica <64868274+neocentrica@users.noreply.github.com>
2022-04-25 21:45:58 -0500

    Add files via upload

---
+++ inf.js
@@ -0,0 +1,8 @@
+function inf() {
+  var x = document.getElementById("infor");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
